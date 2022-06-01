69cfaea6bd4bf878b04d2f8d520e89f9308d3778
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-06-01 15:48:48 +0530

    Updated the windows instaler

--- wininstaller.bat
+++ wininstaller.bat
@@ -1,2 +1,5 @@
 @echo off
 echo Web-Up Windows installer version 1.0.0
+powershell Invoke-WebRequest https://github.com/shourgamer2/web-up/releases/download/web-up/web-up.exe -OutFile .\web-up.exe
+echo "We have download web-up for you now you can use it!"
+pause
